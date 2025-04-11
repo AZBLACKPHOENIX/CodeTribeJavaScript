eee94252ac77cc7d9c9e3b8206ff61df2e2b35aa
AZBLACKPHOENIX <unlimitednigami@gmail.com>
2025-04-11 07:24:31 +0200

    set i =0 before do while 2

--- Conditions/5 do while.js
+++ Conditions/5 do while.js
@@ -1,33 +1,34 @@
 
-i=0;
-do{
- i++;
- console.log(i)
-}while(i<10);
+i = 0;
+do {
+    i++;
+    console.log(i)
+} while (i < 10);
 
 //2
-sum =0
-do{
+sum = 0
+i = 0;
+do {
     i++;
-sum+=i;
-}while(i<100);
+    sum += i;
+} while (i < 100);
 console.log(sum);
 
 //3
-let input=0
+let input = 0
 number = Math.floor(Math.random() * (10))
 console.log(number)
-do{
-    input=prompt("Enter a number greater than 10")
-    if (input>10){
+do {
+    input = prompt("Enter a number greater than 10")
+    if (input > 10) {
         break;
     }
-   }while(input<10);
+} while (input < 10);
 
-  
-   number = Math.floor((Math.random() * 10)+1)
-   console.log(number)
-   
-do{
-    input=prompt("Guess A Number Between 0 And 10")
-   }while(input!=number);
+
+number = Math.floor((Math.random() * 10) + 1)
+console.log(number)
+
+do {
+    input = prompt("Guess A Number Between 0 And 10")
+} while (input != number);
